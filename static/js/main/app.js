bae8863eb51b336c14f00e85bd417330fca461b7
Mahmudumar <mahmudumar476@gmail.com>
2022-06-30 22:42:04 +0100

    finally advanced Alhamdulillah

--- static/js/main/app.js
+++ static/js/main/app.js
@@ -2,13 +2,25 @@ const header = document.querySelector('header');
 const navs = document.querySelector('.navbar');
 const body = document.querySelector('body');
 const burger = document.querySelector('.burger');
+const burgerdivs = document.querySelectorAll('.burger div');
 const navlias = document.querySelectorAll('.navbar li a');
 const navlis = document.querySelectorAll('.navbar li');
+const sections = document.querySelectorAll("section")
+const home = sections[0]
+const about = sections[1]
+const titletext = document.querySelector('.titletext')
+const homehead = document.querySelector('div.homehead')
+const main = document.querySelector('main')
+const logo = document.querySelector('.logo')
 
 function setMinHeight() {
     const headerHeight = header.clientHeight
     const main = document.querySelector('body');
     main.style.setProperty('--nav-height', `${headerHeight}px`);
+};
+const sizeBody = () => {
+    body.setAttribute('width', `${window.innerWidth - navs.clientWidth}px`)
+    body.setAttribute('height', `${window.innerHeight}px`)
 };
 
 const toggle = () => {
@@ -22,15 +34,15 @@ const toggle = () => {
             link.style.animation = `navlisfadein 0.5s ease forwards ${index / 10 + .09}s`
         }
     });
-    burger.classList.toggle('toggle')
+    burger.classList.toggle('toggle');
+    return on;
 }
 
 const clickLinks = (toggle_ = true) => {
         navlias.forEach((a) => {
             const id = a.getAttribute('id')
                 //when clicked scrolls into view
-            const sections = document.querySelector(`.${id}`)
-
+            const sections = document.querySelector(`.${id}` + "-sepblock")
             a.addEventListener('click', () => {
                 sections.scrollIntoView({ 'behavior': 'smooth' })
 
@@ -41,47 +53,69 @@ const clickLinks = (toggle_ = true) => {
         })
     } //toggle nav
 
-
-
-const sizeBody = () => {
-    body.setAttribute('width', `${window.innerWidth - navs.clientWidth}px`)
-    body.setAttribute('height', `${window.innerHeight}px`)
-};
-
 function Scrolling() {
-    //as soon as i start scrolling down, header should go up
-    const yScroll = window.scrollY
-    const scrHeight = document.querySelector('main').scrollHeight
+    //when the home page passes, header should change style
+    const yScr = window.scrollY
+    let mainHeight = body.scrollHeight - window.innerHeight
 
-    if (yScroll >= (scrHeight - yScroll)) {
-        header.style.transform = 'translateY(-100%)'
-        header.style.transition = 'transform 0.5s ease'
-    } else {
-        header.style.transform = ''
+    if (yScr === mainHeight) { //if scroll reaches end
+        //navs
+        navlias.forEach((a) => { a.style.color = '#2e2e2e' })
+            //Brand
+        logo.style.filter = 'brightness(100%)'
+        titletext.style.color = '#2e2e2e'
+            //header
+        header.style.backgroundColor = '#f2f2f2'
+        header.style.boxShadow = ''
+        header.style.transition = 'all 0.2s ease'
+            //if in the main content
+    } else if (yScr >= (60) && yScr < mainHeight) {
+        //navs
+        navlias.forEach((a) => { a.style.color = '#2e2e2e' })
+            //Brand
+        logo.style.filter = 'brightness(100%)'
+        titletext.style.color = '#2e2e2e'
+            //header
+        header.style.backgroundColor = '#f2f2f2'
+        header.style.boxShadow = '0px 2px 20px #2e2e2e'
+        header.style.transition = 'all 0.5s ease'
+            //butger for mobile
+        burgerdivs.forEach(bun => {
+            bun.style.backgroundColor = '#2e2e2e'
+        })
+
+    } else { //if on the top
+        titletext.style.color = ''
+        header.style.backgroundColor = ''
+        header.style.boxShadow = ''
+        logo.style.filter = 'brightness(500%)'
+        navlias.forEach((a) => { a.style.color = '' })
+        burgerdivs.forEach(bun => {
+            bun.style.backgroundColor = ''
+        })
+        header.style.transition = 'all 0.7s ease'
     }
-
 };
-
-
 
 //like listening for media queries
+//function for medi query
+
+
 const media = () => {
 
-    if (window.innerWidth <= 500) {
-        window.removeEventListener('scroll', Scrolling)
-        window.removeEventListener('load', Scrolling)
-        clickLinks(true)
-    } else {
-        window.addEventListener('scroll', Scrolling);
-        window.addEventListener('load', Scrolling);
 
-    }
+
+    clickLinks(true)
+
+    window.addEventListener('scroll', Scrolling);
+    window.addEventListener('load', Scrolling);
+
 }
-
 
 // app functions all together
 const app = () => {
     window.addEventListener('load', sizeBody);
+    window.addEventListener('load', Scrolling);
     window.addEventListener('resize', sizeBody);
     window.addEventListener('resize', media)
     window.addEventListener('load', media)
